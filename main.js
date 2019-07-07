0b2c63bb4e12d77083b675c52e0b056b3c6311cd
JosFun <joshua.funk@tu-ilmenau.de>
2019-07-07 18:36:23 +0200

    Added window on which the user will be able to add new drinks.

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron');
+const url = require('url');
+const path = require('path');
 
 let sql = require('mysql');
+
+/**
+ * The main window of the application
+ * */
+let win;
+
 var connection = sql.createConnection({
     host    : 'localhost',
     user    : 'bhdbuser',
@@ -20,7 +28,7 @@ connection.connect(function(err){
 
 function createWindow () {
     // Erstelle das Browser-Fenster.
-    let win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -32,19 +40,64 @@ function createWindow () {
     // and load the index.html of the app.
     win.loadFile('index.html')
 
+    // Quit app once the main window is closed
+    win.on('closed',function(){
+        app.quit();
+    });
+
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
     Menu.setApplicationMenu(mainMenu);
+}
+
+// Handle createAddWindow
+function createAddDrinkWindow ( ) {
+    windowShrink();
+
+    addWin = new BrowserWindow( {
+        width: 800,
+        height: 600,
+        title:"Add a new Database Entry"
+    });
+
+    addWin.loadURL(url.format({
+        pathname: path.join(__dirname, 'html/addDrinkWindow.html'),
+        protocol:'file',
+        slashes: true
+    }));
 }
 
 function insertTest(){
 
 }
 
+/**
+ * Minimize the main window
+ */
+function windowShrink (){
+    win.minimize();
+}
+
+/**
+ * Maximize the main window
+ */
+function windowExtend (){
+    win.maximize();
+}
+
 // Create a new main menu template
 const mainMenuTemplate = [
     {
-        label: 'File'
+        label: 'File',
+        click(){
+            createAddDrinkWindow();
+        }
+    },
+    {
+        label: 'Quit',
+        click(){
+            app.quit();
+        }
     }
 ];
 
