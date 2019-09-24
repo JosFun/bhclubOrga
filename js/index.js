35a5ba1a3d5ab52ef5c3d899efa6337b113ce646
JosFun <joshua.funk@tu-ilmenau.de>
2019-09-24 14:00:35 +0200

    Made the overlay look better fit on smaller displays than full hd by using css media requests.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
 
 const electron = require('electron');
 const {ipcRenderer} = electron;
-const ul = document.querySelector('ul');
-
-ipcRenderer.on('drink:add', function(e,drink){
-    /* Create a list item */
-    const li = document.createElement('li');
-    const drinkText = document.createTextNode(drink);
-    li.appendChild(drinkText);
-    ul.appendChild(li);
-});
